deaa06a37a45f3ac3f4d5caff1e005285f19a5b8
tristanpendergrass <tristanpendergrass@gmail.com>
2021-06-05 22:52:42 -0700

    Resolve todos in SelectionList

--- src/SelectionList.elm
+++ src/SelectionList.elm
@@ -7,6 +7,8 @@ module SelectionList exposing
     , select
     )
 
+import List.Extra
+
 
 type SelectionList t d
     = SelectionList (List (Maybe t)) (Maybe ( t, d )) (List (Maybe t))
@@ -19,19 +21,70 @@ create =
 
 clearSelection : SelectionList t d -> SelectionList t d
 clearSelection =
-    Debug.todo "Implement clearSelection"
+    toList >> fromList
 
 
-select : Int -> SelectionList t d -> Result String (SelectionList t d)
-select =
-    Debug.todo "Implement select"
+toList : SelectionList t d -> List (Maybe t)
+toList (SelectionList first maybeEl second) =
+    case maybeEl of
+        Nothing ->
+            List.concat
+                [ first, second ]
+
+        Just ( el, _ ) ->
+            List.concat
+                [ first, [ Just el ], second ]
+
+
+fromList : List (Maybe t) -> SelectionList t d
+fromList list =
+    SelectionList list Nothing []
+
+
+select : d -> Int -> SelectionList t d -> Result String (SelectionList t d)
+select initialData index selectionList =
+    let
+        list : List (Maybe t)
+        list =
+            toList selectionList
+
+        first =
+            List.take index list
+
+        maybeEl : Maybe ( t, d )
+        maybeEl =
+            case List.Extra.getAt index list of
+                Just (Just el) ->
+                    Just ( el, initialData )
+
+                _ ->
+                    Nothing
+
+        second =
+            List.drop index list
+    in
+    if index < List.length list then
+        Ok (SelectionList first maybeEl second)
+
+    else
+        Err "Index is out of bounds"
 
 
 mapSelection : (d -> d) -> SelectionList t d -> Result String (SelectionList t d)
-mapSelection =
-    Debug.todo "Implement mapSelection"
+mapSelection fn (SelectionList first maybeEl second) =
+    case maybeEl of
+        Nothing ->
+            Err "Index is out of bounds"
+
+        Just ( el, data ) ->
+            Ok <| SelectionList first (Just ( el, fn data )) second
 
 
 getSelected : SelectionList t d -> Result String ( t, d )
-getSelected =
-    Debug.todo "Implement getSelected"
+getSelected (SelectionList _ maybeEl _) =
+    case maybeEl of
+        Nothing ->
+            Err "Index is out of bounds"
+
+        Just ( el, data ) ->
+            Ok <| ( el, data )
